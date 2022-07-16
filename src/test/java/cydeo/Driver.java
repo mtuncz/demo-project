b1e515bfe2f0c45f38e5ce0bf3dc96a578dd22d4
mtuncz <mehmet.t@icloud.com>
2022-07-16 07:59:22 -0700

    any question guysa

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -38,6 +38,7 @@ public class Driver {
                         String gridAddress = "52.90.101.317";
                         System.out.println("selam sana kara sevdaaaa");
                         System.out.println("yo yeah");
+                        System.out.println("yo yeah");
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
