c0214152c5d1857d016bed4acb85e11df86a9136
mtuncz <mehmet.t@icloud.com>
2022-07-16 07:45:29 -0700

    added sth2

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -36,7 +36,7 @@ public class Driver {
                     try {
                         // assign your grid server address
                         String gridAddress = "52.90.101.317";
-                        System.out.println("selam sana kara sevda");
+                        System.out.println("selam sana kara sevdaaaa");
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
